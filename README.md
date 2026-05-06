31f2331c71b7aaff821003f1584e5022f95aaefd
Miguel Angel Munoz Vazquez <mig1-0001@hotmail.com>
2026-05-05 20:16:19 -0600

    base script and a simple manual test

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Proof-Of-Concept-Hotel-AI-Agent
 A proof of concept of an AI Hotel Assistant that handles various guest queries in a professional but friendly manner. This agent is built on top of Microsoft Foundry (https://azure.microsoft.com/en-us/products/ai-foundry) as part of my completion of certification course called AI agent fundamentals with Azure AI Foundry.
 ## (Pending) How to set up/install
+
+- pip3 install azure-ai-projects==2.1.0
+
+
 ## (Pending) Example usage
 
 
--- src/main.py
+++ src/main.py
@@ -1,1 +1,45 @@
+from py_compile import main
+import sys
 
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.core.credentials import AzureKeyCredential
+
+
+endpoint = "https://foundry-1-learn-2.services.ai.azure.com/api/projects/proj-default"
+
+project_client = AIProjectClient(
+    endpoint=endpoint,
+    credential=DefaultAzureCredential(),
+)
+
+my_agent = "CrystalHotelsAssistant-KnowledgeSystemImplementation"
+my_version = "6"
+
+openai_client = project_client.get_openai_client()
+
+
+def main():
+    messageNumber = 0
+    while(1):
+        messageNumber += 1
+        message = input(f"({messageNumber}) Type your message and press Enter: ")
+        
+
+        # Get user input
+        print(f"Received message: {message}")
+
+        # Reference the agent to get a response
+        response = openai_client.responses.create(
+            input=[{"role": "user", "content": message}],
+            extra_body={"agent_reference": {"name": my_agent, "version": my_version, "type": "agent_reference"}},
+        )
+
+        messageNumber += 1
+        print(f"({messageNumber}) Response: {response.output_text}")
+
+# TODO:
+# Add local throttle to avoid hitting rate limits, specially if user want to spam messages.
+# Add way to handle images.
+# Fix the agent to actually not reask the user for the same information multiple times, specially if the user already provided it in the conversation, like guest amount.
+main()
